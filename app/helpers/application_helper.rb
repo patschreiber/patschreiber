c43523797c536343980fe2c66b72736645d54250
patricks <patrick@nori.nu>
2014-03-09 01:25:53 -0600

    Added profile pics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,16 @@ module ApplicationHelper
   def current_projects
     @projects = Project.order(:end_date).all
   end
+
+  def twitter_url
+    "https://twitter.com/patschreiber"
+  end
+
+  def github_url
+    "https://github.com/patschreiber"
+  end
+
+  def linkedin_url
+    "http://www.linkedin.com/pub/patrick-schreiber/2a/85/833/"
+  end
 end
