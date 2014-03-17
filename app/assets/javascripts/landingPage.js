58e231649bc821a79b6d05f33c85a6e4b67a6d07
patricks <patrick@nori.nu>
2014-03-17 15:56:07 -0500

    jquery menu collapse work

--- app/assets/javascripts/landingPage.js
+++ app/assets/javascripts/landingPage.js
@@ -6,12 +6,21 @@ $(document).ready(function() {
 
   var animationDone = true;
   
-  $(".clicktest").click(function() {
-
+  $(".nav-expand").click(function() {
     if( $(".nav-menu").css("display") != 'none' ) {
       if(animationDone) {
         animationDone = false;
         $(".nav-menu").stop(true, true).toggle('slide', 400);
+
+        $(".nav-expand").stop(true, true).animate({
+          "left" : "0",
+          "width" : "100%",
+        }, function() {
+          $(this).animate({"height" : "10px"}, function() {
+            $(this).addClass("collapsed");
+          });
+        });
+
         $(".content").stop(true, true).animate({"margin-left": "0"}, function() {
           animationDone = true;
         });
@@ -21,9 +30,33 @@ $(document).ready(function() {
       if(animationDone) {
         animationDone = false;
         $(".content").stop(true, true).animate({"margin": "0 0 0 300px"});
+        $(".nav-expand").stop(true, true).animate({
+          "left" : "315px",
+          "width" : "50px",
+          "height" : "50px"
+        }, function() {
+          $(this).removeClass("collapsed");
+        });
         $(".nav-menu").stop(true, true).toggle('slide', 400, function() {
           animationDone = true;
         });
+      }
+    }
+  });
+
+
+  $(".nav-expand").on("mouseenter", function() {
+    if( $(this).hasClass("collapsed") ) {
+      $(this).stop().animate({"height" : "50px"}, function() {
+        $(".collapsed-nav").fadeIn();
+      });
+    }
+  });
+
+  $(".nav-expand").on("mouseleave", function() {
+    if( $(this).hasClass("collapsed") ) {
+      if(animationDone) {
+        $(this).stop().animate({"height" : "10px"});
       }
     }
   });
