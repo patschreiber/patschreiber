f72a445d0ed46235f97e1505c45ff0d41b887878
patricks <patrick@nori.nu>
2014-03-16 23:10:39 -0500

    Added slide for main menu

--- app/assets/javascripts/landingPage.js
+++ app/assets/javascripts/landingPage.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
-$(".nav-menu").bind("mousewheel", function (ev, delta) {
-    var scrollTop = $(window).scrollTop();
-    $(window).scrollTop(scrollTop - Math.round(delta * 20));
-});
+  $(".nav-menu").bind("mousewheel", function (ev, delta) {
+      var scrollTop = $(window).scrollTop();
+      $(window).scrollTop(scrollTop - Math.round(delta * 20));
+  });
+
+
+  $(".clicktest").click(function() {
+
+    if( $(".nav-menu").css("display") != 'none' ) {
+      $(".nav-menu").toggle("slide", function() {
+        $(".content").css({"margin": "0 auto"});
+      });
+    }
+    else {
+      $(".nav-menu").toggle("slide", function() {
+        $(".content").css({"margin": "0 0 0 300px"});
+      });      
+    }
+  });
 });
