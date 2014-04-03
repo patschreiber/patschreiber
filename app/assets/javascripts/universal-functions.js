304cca94e60465ac6e913a154432991e0ff16a37
patricks <patrick@nori.nu>
2014-04-03 11:50:57 -0500

    Crushed cookie setter bug in IE. The Regex used = bad

--- app/assets/javascripts/universal-functions.js
+++ app/assets/javascripts/universal-functions.js
@@ -1,24 +1,17 @@
-  function setCookie( cookie_name, cookie_value, lifespan_in_days, valid_domain ) {
+function setCookie( cookie_name, cookie_value, lifespan_in_days, valid_domain ) {
+  var domain_string = valid_domain ? ("; domain=" + valid_domain) : '';
+  
+  document.cookie = cookie_name + "=" + encodeURIComponent( cookie_value ) + "; max-age=" + 60 * 60 * 24 * lifespan_in_days + "; path=/" + domain_string;
+}
+
+function getCookie( cookie_name ) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + cookie_name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+//[ cookie_name, valid_domain(optional) ]
+function deleteCookie( cookie_name, valid_domain ) {
     var domain_string = valid_domain ? ("; domain=" + valid_domain) : '';
-    document.cookie = cookie_name +
-                       "=" + encodeURIComponent( cookie_value ) +
-                       "; max-age=" + 60 * 60 *
-                       24 * lifespan_in_days +
-                       "; path=/" + domain_string;
-  }
-
-  function getCookie( cookie_name ) {
-    var cookie_string = document.cookie;
-    if (cookie_string.length != 0) {
-      var cookie_value = cookie_string.match('(^|;)[\s]*' + cookie_name + '=([^;]*)' );
-      return decodeURIComponent( cookie_value[2] );
-    }
-
-    return '';
-  }
-
-  //[ cookie_name, valid_domain(optional) ]
-  function deleteCookie( cookie_name, valid_domain ) {
-      var domain_string = valid_domain ? ("; domain=" + valid_domain) : '';
-      document.cookie = cookie_name +"=; max-age=0; path=/" + domain_string;
-  }
+    document.cookie = cookie_name +"=; max-age=0; path=/" + domain_string;
+}
