3e12921ca0a902cdceb8333492cf1b06877e4794
patricks <patrick@nori.nu>
2014-04-10 00:19:54 -0500

    fixed arrows. svg only. no need for png fallback as not supporting ie8

--- app/assets/javascripts/modernizrTests.js
+++ app/assets/javascripts/modernizrTests.js
@@ -7,9 +7,4 @@ else {
   console.log("no");
 }
 
-if(!Modernizr.svg) {
-  $('.nav-menu-arrow-top').attr("src", "arrow-up.png");
-  $('.nav-menu-arrow-bottom').attr("src", "arrow-down.png");
-}
-
 });
