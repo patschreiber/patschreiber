d95fed3b08c2826cddc38cabf2fb86d34560ccf8
patricks <patrick@nori.nu>
2014-11-10 17:08:23 -0600

    Updated some content for projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,19 +22,28 @@ lambda {
   incense.url = 'http://labs.nori.nu/'
   incense.repo_url = 'https://github.com/patschreiber/incense'
   incense.start_date = '2014-01-20'
-  incense.end_date = nil
+  incense.end_date = '2014-07-24'
   incense.save!
 }.call
 
 lambda {
   nori = Project.new
   nori.title = 'Nori'
-  nori.slug = 'Nori is a game about collecting, trading, and improving. Collect items. Play with your friends. Trade around the world.'
+  nori.slug = 'Nori is a game about collecting, trading, and improving. Collect items. Play with your friends. Trade around the world. '
   nori.technologies = 'Ruby on Rails 4, MySQL, Capistrano, HTML, CSS, jQuery, redis, Resque, cron'
+  nori.expanded_info = <<-END
+  > The project was interesting as it was my first forray into the world of Rails development. I wanted to create a 'compulsive clicking game' that was infectious to play. It borrowed heavily from Diablo 2 (PC) and Cookie Clicker (Flash, PC, Mac). The objective was to collect the best items in the game, either through hard work or smart trading.
+  >
+  > The project itself was a mess. As a 'first' web application project, the codebase left a little to be desired, which is as expected, however, aside from that the feature management was all over the place. I started creating features with no real goal. It was really nose to the grindstone from the beginning. I had an *idea* about what I wanted from the end result, but there was no regard to how features would intelligently interact. This led to a jumbled mess of models and their purpose, unnecessary ajax calls, and overall, really complicated javascript to play with Bootstrap. 
+  >
+  > If I were to start the project over, I would plan on setting out the base framework, insofar as to have basic interactions laid out. I would also try to encapsulate each action a little more. Another key place that I would improve upon would be the design. I knew I wanted it "dark," like some of the source material it was referencing, however, there was no direction. If one uses greys upon greys upon greys, their UI is going to be very hard to navigate. What are 'actionable' greys, what are not. What dark color is a 'call to action?' Does it successfully grab the users attention? What about the layouts of the information? It was all thrown together, really. The idea was to iterate and iterate, but the initial complexity added by css override classes were just compounded the larger the project got. 
+  >
+  > Ultimately the skills learned and objectives of the project were rolled into a new project that is structured far better than Nori. RIP."
+  END
   nori.url = 'http://nori.nu/'
   nori.repo_url = 'https://github.com/patschreiber/nori.nu'
   nori.start_date = '2013-07-30'
-  nori.end_date = nil
+  nori.end_date = '2014-05-23'
   nori.save!
 }.call
 
@@ -44,10 +53,8 @@ lambda {
   oilman.slug = 'Compulsive clicking game. The project stipulations were to make a game over a long holiday weekend. Constraints included feature management due to time as well as quick prototyping and execution'
   oilman.technologies = 'HTML, CSS, jQuery'
   oilman.expanded_info = <<-END
-  > Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-  > Lorem Ipsum is simply dummy text of the printing anith desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-  >
-  > Yes sir thats the desc
+  > I had a lot of fun with Oilman. I was with relatives for a weekend, so I decided to see if I couldn't complete a project within a strict deadline. This deadline was 2 days with limited internet and limited time during the day to code. The product of this 'challenge' is a little game where the player tries to build up their oil empire and ultimately pay back the loan shark, who the player owes money.
+  > The project is pure HTML, CSS, and jQuery. A lot of the actions in the game are dependent on setInterval, and other javascript event handlers. Unfortunately due to the time constraints, the balance in the game is way off. With a little more time I feel the replayability would be increased tenfold.
   END
   oilman.url = 'http://oilman.labs.nori.nu/'
   oilman.repo_url = 'https://github.com/patschreiber/oilman/'
@@ -86,6 +93,11 @@ lambda {
   END
   blog.save!
 }.call
+
+lambda {
+  blog = Blog.new
+  blog.title = "Adaptive Interfaces, or, the media queries of the future."
+}
 
 
 puts "Finished seeding the database!"
