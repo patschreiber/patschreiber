51be4e462a38038894921efd73269ccf37e841f0
Patrick Schreiber <ptos382@gmail.com>
2019-12-12 18:57:01 -0600

    Fixing Mozilla Observatory issues

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-PatschreiberCom::Application.config.session_store :cookie_store, key: '_patschreiber_com_session'
+PatschreiberCom::Application.config.session_store :cookie_store,
+  key: '_ps_sess',
+  secure: Rails.env.production?,
+  same_site: :strict
