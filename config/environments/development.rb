abb8bfdf30e3b928e0e53833004dbbc503f689c7
Patrick Schreiber <ptos382@gmail.com>
2019-12-29 05:22:40 -0600

    App update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,6 +22,7 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -35,7 +36,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for
   # options).
-  # config.active_storage.service = :local
+  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
