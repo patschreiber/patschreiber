ea3c82fba3b2f1918b4bc59f6ce22318fc6ec462
Patrick Schreiber <ptos382@gmail.com>
2020-03-05 00:55:08 -0600

    Added vendor legal and blank page

--- app/controllers/legal_controller.rb
+++ app/controllers/legal_controller.rb
@@ -8,4 +8,11 @@ class LegalController < ApplicationController
   def terms; end
 
   def privacy_policy; end
+
+  #
+  # Static route for the yarn-generated vendor licenses.
+  #
+  def third_party_licenses
+    @license_file = File.read(Rails.root.join('public/VENDOR-LICENSES'))
+  end
 end
